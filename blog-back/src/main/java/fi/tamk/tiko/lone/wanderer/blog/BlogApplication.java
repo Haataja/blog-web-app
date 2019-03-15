8baba78391d22c2c53c80aac528d196eaf19804e
Haataja <hanna.haataja@cs.tamk.fi>
2019-03-15 16:13:38 +0200

    Added instructions when starting backend

--- blog-back/src/main/java/fi/tamk/tiko/lone/wanderer/blog/BlogApplication.java
+++ blog-back/src/main/java/fi/tamk/tiko/lone/wanderer/blog/BlogApplication.java
@@ -1,13 +1,22 @@
 package fi.tamk.tiko.lone.wanderer.blog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
+    public static String BASEURL = "http://localhost:8080/";
+    public static String RESOURCE = "posts/";
+
+    private static Logger log = LoggerFactory.getLogger(BlogApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
+        log.info("INSTRUCTIONS");
+        log.info("------------");
+        log.info("GET all blog posts curl -X GET {}{}",BASEURL,RESOURCE);
     }
 
 }
